0a9a24a4fbb075ea4325a9e10bee595904f9dcce
nholohan <nholohan@tcd.ie>
2018-05-25 12:48:02 +0100

    cleaning code

--- war-api.py
+++ war-api.py
@@ -2,7 +2,6 @@ import sys, os
 sys.path.append(os.path.join(os.getcwd(),'python/'))
 
 import darknet as dn
-import pdb
 import time
 import json
 from flask import Flask, request, Response, send_file
@@ -41,8 +40,6 @@ def calculate_complexity():
     fh.write(r.data)
     fh.close()
 
-    # start = time.time()
-    # result = tfnet.return_predict(img_np)
     boxes = dn.detect(net, meta, "./testImage.jpg")
     print(boxes)
 
@@ -69,35 +66,7 @@ def calculate_complexity():
                 "y": bottom_y}
         })
 
-    # TODO: change to x,y,w,h
-    # boxesInfo = list()
-    # for box in boxes:
-    #     boxesInfo.append({
-    #         "label": box[0],
-    #         "confidence": box[1],
-    #         "topleft": {
-    #             "x": box[2][0],
-    #             "y": box[2][1]},
-    #         "bottomright": {
-    #             "x": box[2][2],
-    #             "y": box[2][3]}
-    #     })
-    # print(start - time.time())
-    #
     json_result = json.dumps(boxesInfo)
-
-    # print(result)
-
-    # for obj in result:
-    #     obj['confidence'] = str(obj['confidence'])
-    #
-    # json_result = json.dumps(result)
-    #
-    # print(json_result)
-
-    # example_json = {"topleft": {"y": 124, "x": 76}, "confidence": "0.793635", "bottomright": {"y": 455, "x": 556}, "label": "bicycle"}
-    # json_eg = json.dumps(example_json)
-    # print(json_eg)
     print(start - time.time())
     return Response(response=json_result, status=200, mimetype="application/json")
 
